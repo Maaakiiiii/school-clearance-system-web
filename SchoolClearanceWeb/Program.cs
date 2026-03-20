fac355a3fdb14426de916fadd8a23cc5d88f95d9
Johanna Marie Inso <johannamarieinso31@gmail.com>
2026-03-20 20:48:20 +0800

    UI Design - School Clearance Web

---
+++ SchoolClearanceWeb/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using SchoolClearanceWeb;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices(); // Mudblazor services
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
