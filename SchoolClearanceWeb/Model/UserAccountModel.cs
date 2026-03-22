09a30eadee841cd210c4ddf78177e4760f2dace5
Johanna Marie Inso <johannamarieinso31@gmail.com>
2026-03-22 03:55:34 -0400

    March 22, 2026 -- ADDED TEST PUSH FILE

---
+++ SchoolClearanceWeb/Model/UserAccountModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SchoolClearanceWeb.Model
+{
+
+    public class UserAccountModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; } 
+        public string Username { get; set; } 
+        public string Password { get; set; }
+        
+    }
+
+}
--- SchoolClearanceWeb/Program.cs
+++ SchoolClearanceWeb/Program.cs
@@ -9,7 +9,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices(); // Mudblazor services
+builder.Services.AddSingleton<UserService>(); // User Service for short term memory store
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+ 
 await builder.Build().RunAsync();
